f2693af44919d4e0a664a5973649ab83640a5a52
paraigor <icid0096@yandex.ru>
2024-08-09 17:28:46 +0300

    Reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -27,7 +27,7 @@ def serialize_post_optimized(post):
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
-        "tags": [serialize_tag(tag) for tag in post.tags.all()],
+        "tags": [tag.title for tag in post.tags.all()],
         "first_tag_title": post.tags.all()[0].title,
     }
 
@@ -35,7 +35,7 @@ def serialize_post_optimized(post):
 def serialize_tag(tag):
     return {
         "title": tag.title,
-        "posts_with_tag": len(Post.objects.filter(tags=tag)),
+        "posts_with_tag": tag.num_posts,
     }
 
 
@@ -95,7 +95,7 @@ def post_detail(request, slug):
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
-        "tags": [serialize_tag(tag) for tag in related_tags],
+        "tags": [tag.title for tag in related_tags],
     }
 
     most_popular_tags = Tag.objects.popular()[:5]
